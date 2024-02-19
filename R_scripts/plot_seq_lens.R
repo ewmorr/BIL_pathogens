fdfda9b5dac42ddb3808d9a3ae55408319be4917
Eric Morrison <emorrison40@gmail.com>
2024-02-19 13:49:36 -0500

    dada2 core alg

--- R_scripts/plot_seq_lens.R
+++ R_scripts/plot_seq_lens.R
@@ -1,11 +1,23 @@
 library(dplyr)
 library(ggplot2)
 
-lens = read.table("data/vol_test/all.lens.txt")
-lens_mod = read.table("data/vol_test/all_mod.lens.txt")
+lens = read.table("data/vol_test/itsx_test/R1.lens.txt")
+lens_mod = read.table("data/vol_test/itsx_mod_test/R1.lens.txt")
 
-comb_lens = left_join(lens, lens_mod, by = "V1") 
+comb_lens = left_join(lens[c("V1","V3")], lens_mod[c("V1","V3")], by = "V1") 
+head(comb_lens)
+cor(comb_lens$V3.x, comb_lens$V3.y)
+
+ggplot(comb_lens, aes(V3.x, V3.y)) +
+    geom_point()
+
+lens = read.table("data/vol_test/itsx_test/R2.lens.txt")
+lens_mod = read.table("data/vol_test/itsx_mod_test/R2.lens.txt")
+
+comb_lens = left_join(lens[c("V1","V3")], lens_mod[c("V1","V3")], by = "V1") 
 head(comb_lens)
 
-ggplot(comb_lens, aes(V2.x, V2.y)) +
+ggplot(comb_lens, aes(V3.x, V3.y)) +
     geom_point()
+
+cor(comb_lens$V3.x, comb_lens$V3.y)
