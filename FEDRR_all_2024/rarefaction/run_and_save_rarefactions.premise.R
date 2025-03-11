027a53651170ed57ec68271d01043f157ae14fde
Eric Morrison <emorrison40@gmail.com>
2025-03-11 14:30:49 -0400

    rarefactions

--- FEDRR_all_2024/rarefaction/run_and_save_rarefactions.premise.R
+++ FEDRR_all_2024/rarefaction/run_and_save_rarefactions.premise.R
@@ -1,25 +1,40 @@
 library(dplyr)
 source("~/repo/BIL_pathogens/library/library.R")
 
-#asv_tab.baseline = read.csv("~/FEDRR_all_2024/rarefactions/asv_tab.baseline.csv", header = T, row.names = 1)
-#asv_tab.pcr = read.csv("~/FEDRR_all_2024/rarefactions/asv_tab.pcr.csv", header = T, row.names = 1)
-asv_tab.samps = read.csv("~/FEDRR_all_2024/rarefactions/asv_tab.funnel_traps_and_spore_traps.csv", header = T, row.names = 1)
-#asv_tab.samps_and_baseline = read.csv("~/FEDRR_all_2024/rarefactions/asv_tab.samps_and_baseline.csv", header = T, row.names = 1)
+asv_tab.samps = read.csv("~/FEDRR_all_2024/rarefactions/asv_tab.samps.csv", header = T, row.names = 1)
+asv_tab.samps_and_baseline = read.csv("~/FEDRR_all_2024/rarefactions/asv_tab.samps_and_baseline.csv", header = T, row.names = 1)
+asv_tab.samps_and_pcrNeg = read.csv("~/FEDRR_all_2024/rarefactions/asv_tab.samps_and_pcrNeg.csv", header = T, row.names = 1)
+asv_tab.samps_and_baseline.NH = read.csv("~/FEDRR_all_2024/rarefactions/asv_tab.samps_and_baseline.NH.csv", header = T, row.names = 1)
+asv_tab.baseline.NH = read.csv("~/FEDRR_all_2024/rarefactions/asv_tab.baseline.NH.csv", header = T, row.names = 1)
+asv_tab.samps.NH = read.csv("~/FEDRR_all_2024/rarefactions/asv_tab.samps.NH.csv", header = T, row.names = 1)
+#asv_tab.samps = read.csv("~/FEDRR_all_2024/rarefactions/asv_tab.funnel_traps_and_spore_traps.csv", header = T, row.names = 1)
 
 
 
-#min_seqs.baseline = min(rowSums(asv_tab.baseline))
-#rarefactions_list.baseline = multiple_subsamples(x = asv_tab.baseline, depth = min_seqs.baseline, iterations = 1000)
-#saveRDS(rarefactions_list.baseline, "~/FEDRR_all_2024/rarefactions/rarefactions.baseline.rds")
+min_seqs.samps = min(rowSums(asv_tab.samps))
+rarefactions_list.samps = multiple_subsamples(x = asv_tab.samps, depth = min_seqs.samps, iterations = 1000)
+saveRDS(rarefactions_list.samps, "~/FEDRR_all_2024/rarefactions/rarefactions.samps.rds")
 
-#min_seqs.pcr = min(rowSums(asv_tab.pcr))
-#rarefactions_list.pcr = multiple_subsamples(x = asv_tab.pcr, depth = min_seqs.pcr, iterations = 1000)
-#saveRDS(rarefactions_list.pcr, "~/FEDRR_all_2024/rarefactions/rarefactions.pcr.rds")
+min_seqs.samps_and_baseline = min(rowSums(asv_tab.samps_and_baseline))
+rarefactions_list.samps_and_baseline = multiple_subsamples(x = asv_tab.samps_and_baseline, depth = min_seqs.samps_and_baseline, iterations = 1000)
+saveRDS(rarefactions_list.samps_and_baseline, "~/FEDRR_all_2024/rarefactions/rarefactions.samps_and_baseline.rds")
 
-min_seqs.samps = min(rowSums(asv_tab.samps)) 
-rarefactions_list.samps = multiple_subsamples(x = asv_tab.samps, depth = min_seqs.samps, iterations = 1000) 
-saveRDS(rarefactions_list.samps, "~/FEDRR_all_2024/rarefactions/rarefactions.funnel_traps_and_spore_traps.rds")
+min_seqs.samps_and_pcrNeg = min(rowSums(asv_tab.samps_and_pcrNeg))
+rarefactions_list.samps_and_pcrNeg = multiple_subsamples(x = asv_tab.samps_and_pcrNeg, depth = min_seqs.samps_and_pcrNeg, iterations = 1000)
+saveRDS(rarefactions_list.samps_and_pcrNeg, "~/FEDRR_all_2024/rarefactions/rarefactions.samps_and_pcrNeg.rds")
 
-#min_seqs.samps_and_baseline = min(rowSums(asv_tab.samps_and_baseline))
-#rarefactions_list.samps_and_baseline = multiple_subsamples(x = asv_tab.samps_and_baseline, depth = min_seqs.samps_and_baseline, iterations = 1000)
-#saveRDS(rarefactions_list.samps_and_baseline, "~/FEDRR_all_2024/rarefactions/rarefactions.samps_and_baseline.rds")
+min_seqs.samps_and_baseline.NH = min(rowSums(asv_tab.samps_and_baseline.NH))
+rarefactions_list.samps_and_baseline.NH = multiple_subsamples(x = asv_tab.samps_and_baseline.NH, depth = min_seqs.samps_and_baseline.NH, iterations = 1000)
+saveRDS(rarefactions_list.samps_and_baseline.NH, "~/FEDRR_all_2024/rarefactions/rarefactions.samps_and_baseline.NH.rds")
+
+min_seqs.baseline.NH = min(rowSums(asv_tab.baseline.NH))
+rarefactions_list.baseline.NH = multiple_subsamples(x = asv_tab.baseline.NH, depth = min_seqs.baseline.NH, iterations = 1000)
+saveRDS(rarefactions_list.baseline.NH, "~/FEDRR_all_2024/rarefactions/rarefactions.baseline.NH.rds")
+
+min_seqs.samps.NH = min(rowSums(asv_tab.samps.NH))
+rarefactions_list.samps.NH = multiple_subsamples(x = asv_tab.samps.NH, depth = min_seqs.samps.NH, iterations = 1000)
+saveRDS(rarefactions_list.samps.NH, "~/FEDRR_all_2024/rarefactions/rarefactions.samps.NH.rds")
+
+#min_seqs.samps = min(rowSums(asv_tab.samps))
+#rarefactions_list.samps = multiple_subsamples(x = asv_tab.samps, depth = min_seqs.samps, iterations = 1000)
+#saveRDS(rarefactions_list.samps, "~/FEDRR_all_2024/rarefactions/rarefactions.funnel_traps_and_spore_traps.rds")
